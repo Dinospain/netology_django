756350e5550608f86b356e064973195cf9f58b1e
wolqemor1 <0v0v0v0@mail.ru>
2022-11-05 16:50:19 +0300

    registered view function wrote a handle

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,6 +19,7 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
@@ -28,3 +29,12 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+def recipes(request, recipe):
+    number_servings = int(request.GET.get('servings', 1))
+    data = {ingredient: round(amount * number_servings, 2) for ingredient, amount in DATA[recipe].items()}
+
+    context = {
+        'recipe': data
+    }
+    return render(request, 'calculator/index.html', context)
