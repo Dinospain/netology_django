38f3bdc7ed7165bcf9e87edd7cf1e0e996d372d8
wolqemor1 <0v0v0v0@mail.ru>
2022-12-02 19:44:48 +0300

    add model "phone" add command

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.DateField(blank=True, null=True)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
