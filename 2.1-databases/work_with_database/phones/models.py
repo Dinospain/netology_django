6126ea46c756dead2a6cce8a1d6def081cd12f15
wolqemor1 <0v0v0v0@mail.ru>
2022-12-14 19:21:16 +0300

    add migrations

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Phone(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, null=True)
     price = models.IntegerField()
     image = models.ImageField()
     release_date = models.DateField(blank=True, null=True)
     lte_exists = models.BooleanField()
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=150)
