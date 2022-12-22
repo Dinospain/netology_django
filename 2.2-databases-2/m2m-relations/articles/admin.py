65f1d3bf70779a4cfff486b2e70eb42d77c79c3f
wolqemor1 <0v0v0v0@mail.ru>
2022-12-22 17:30:12 +0300

    add task

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name'
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count += 1
+            if count > 1:
+                raise ValidationError('Основным может быть только один раздел')
+        if count == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 2
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'text', 'published_at', 'image']
+    list_filter = ['published_at']
+    inlines = [ScopeInline]
