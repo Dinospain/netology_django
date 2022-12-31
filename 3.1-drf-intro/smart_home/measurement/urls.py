051c3f1faa9636e92d94319e296d34a48323125d
wolqemor1 <0v0v0v0@mail.ru>
2022-12-31 18:28:32 +0300

    add models, serializers, urls, views

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', CreateAPIView.as_view()),
+    path('sensors/<pk>/', RetrieveUpdateAPIView.as_view()),
+    path('measurements/', ListCreateAPIView.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,42 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+
+from .models import Sensor, Measurement
+from .serializers import MeasurementSerializer, SensorDetailSerializer
+
+
+class ListCreateAPIView(ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request):
+        review = MeasurementSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+
+        return Response({'status': 'OK'})
+
+
+class RetrieveUpdateAPIView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def patch(self, request, pk):
+        sensor = Sensor.objects.get(pk=pk)
+        serializer = SensorDetailSerializer(sensor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+        return Response(serializer.data)
+
+
+class CreateAPIView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def post(self, request):
+        review = SensorDetailSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+
+        return Response({'status': 'OK'})
